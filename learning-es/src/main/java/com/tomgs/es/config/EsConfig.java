2e102e57f7f6b55a1618ee627755d5d2ba808662
tangzhongyuan <tangzhongyuan@globalegrow.com>
2019-04-24 16:04:22 +0800

    update

--- learning-es/src/main/java/com/tomgs/es/config/EsConfig.java
+++ learning-es/src/main/java/com/tomgs/es/config/EsConfig.java
@@ -4,8 +4,9 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,17 +21,12 @@ import java.net.UnknownHostException;
  * @since 2019-04-04 18:23
  **/
 @Configuration
+@EnableConfigurationProperties(EsProperties.class)
 @ComponentScan("com.tomgs.es.*")
 public class EsConfig {
 
-    @Value("${es.cluster.name}")
-    private String clusterName;
-
-    @Value("${es.host}")
-    private String host;
-
-    @Value("${es.port}")
-    private int port;
+    @Autowired
+    private EsProperties esProperties;
 
     @Bean(destroyMethod = "close")
     @ConditionalOnMissingBean(TransportClient.class)
@@ -39,7 +35,7 @@ public class EsConfig {
         // 指定集群名,默认为elasticsearch,如果改了集群名,这里一定要加
         Settings settings = Settings.builder()
                 //设置ES实例的名称
-                .put("cluster.name", clusterName)
+                .put("cluster.name", esProperties.getClusterName())
                 //自动嗅探整个集群的状态，把集群中其他ES节点的ip添加到本地的客户端列表中
                 //.put("client.transport.sniff", true)
                 .build();
@@ -51,10 +47,10 @@ public class EsConfig {
         这里只配置了一个节点的地址然添加进去,也可以配置多个从节点添加进去再返回
          */
         TransportAddress node = new TransportAddress(
-                InetAddress.getByName(host), port);
+                InetAddress.getByName(esProperties.getHost()), esProperties.getPort());
         client.addTransportAddress(node);
 
-        System.out.println("连接" + host + ":" + port + "成功...");
+        System.out.println("连接" + esProperties.getHost() + ":" + esProperties.getPort() + "成功...");
 
         return client;
     }
