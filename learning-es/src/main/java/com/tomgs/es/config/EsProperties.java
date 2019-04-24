8ff21cf732d25c6a25d270a696154363db40f2e1
tangzhongyuan <tangzhongyuan@globalegrow.com>
2019-04-24 16:04:22 +0800

    update

---
+++ learning-es/src/main/java/com/tomgs/es/config/EsProperties.java
@@ -0,0 +1,30 @@
+package com.tomgs.es.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author tangzhongyuan
+ * @since 2019-04-24 15:50
+ **/
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "es")
+public class EsProperties {
+
+    //@Value("${es.cluster-name}")
+    private String clusterName;
+
+    //@Value("${es.host}")
+    private String host;
+
+    //@Value("${es.port}")
+    private int port;
+
+}
+
