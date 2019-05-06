97ab493995a1b8ecc1003d1307fa92e925445b5b
tincopper <136082619@qq.com>
2019-05-06 23:40:41 +0800

    add action module

---
+++ learning-esway/src/main/java/com/tomgs/es/gateway/module/ActionModule.java
@@ -0,0 +1,17 @@
+package com.tomgs.es.gateway.module;
+
+import com.google.inject.AbstractModule;
+
+/**
+ *  action主从module
+ *
+ * @author tomgs
+ * @version 2019/5/6 1.0 
+ */
+public class ActionModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
