90fe12a3c23df4ade2f3e0de050e028ace75a551
tangzhongyuan <tangzhongyuan@globalegrow.com>
2019-07-16 11:09:34 +0800

    update all

---
+++ learning-core/src/main/java/com/tomgs/core/java8/promise/CompletableFutureTest.java
@@ -0,0 +1,14 @@
+package com.tomgs.core.java8.promise;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author tangzhongyuan
+ * @since 2019-07-15 9:31
+ **/
+public class CompletableFutureTest {
+
+    public static void main(String[] args) {
+        //CompletableFuture.supplyAsync();
+    }
+}
