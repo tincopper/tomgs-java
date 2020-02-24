8d96f435148dc4bf2727b825e0b31783a36232a9
tomgs <tincopper@foxmail.com>
2020-02-24 17:06:03 +0800

    update unused import

--- learning-sentinel/sentinel-token-server/src/main/java/com/tomgs/sentinel/cluster/server/parse/TokenServerTransportConfigParser.java
+++ learning-sentinel/sentinel-token-server/src/main/java/com/tomgs/sentinel/cluster/server/parse/TokenServerTransportConfigParser.java
@@ -6,7 +6,7 @@ import com.alibaba.csp.sentinel.log.RecordLog;
 import com.alibaba.csp.sentinel.util.HostNameUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import com.chenyin.sentinel.token.server.entity.ClusterGroupEntity;
+import com.tomgs.sentinel.cluster.server.entity.ClusterGroupEntity;
 
 import java.util.List;
 
