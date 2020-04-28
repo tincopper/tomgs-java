47ef22098d572ea3164011dec4f0d8f340a13f4d
zy_tang <zy_tang@kingdee.com>
2020-04-28 20:21:26 +0800

    更新scheduelr

--- learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/Main2.java
+++ learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/Main2.java
@@ -1,22 +1,21 @@
 package com.tomgs.scheduler.quartz.customer;
 
+import com.tomgs.scheduler.quartz.customer.spi.QuartzScheduler;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ServiceLoader;
 
 /**
  * @author tangzy
  * @since 1.0
  */
-public class Main {
+public class Main2 {
 
   public static void main(String[] args) throws Exception {
-    BasicScheduler bs = (BasicScheduler)readObject();
+    // 这个序列化没啥用，序列化之后调度器里面的任务是空的。
+    QuartzScheduler bs = readObject();
     if (bs != null) {
       bs.start();
     } else {
@@ -32,12 +31,16 @@ public class Main {
   }
 
   // 这种方式不行，这种一定要实现Serializable接口
-  public static Object readObject() {
+  public static QuartzScheduler readObject() {
     FileInputStream fis;
     try {
       fis = new FileInputStream(new File("D://scheduler.tmp"));
-      ObjectInputStream ois = new ObjectInputStream(fis);
-      return ois.readObject();
+      byte[] bytes = new byte[2048];
+      int read;
+      while ((read = fis.read(bytes)) != -1) {
+        System.out.println(read);
+      }
+      return SerializeUtil.deserialize(bytes, QuartzScheduler.class);
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -45,10 +48,11 @@ public class Main {
   }
 
   public static void writeObject(Object o) throws IOException {
+    byte[] serialize = SerializeUtil.serialize(o);
     FileOutputStream fos = new FileOutputStream(new File("D://scheduler.tmp"));
-    ObjectOutputStream oos = new ObjectOutputStream(fos);
-    oos.writeObject(o);
-    oos.close();
+    fos.write(serialize);
+    fos.flush();
+    fos.close();
   }
 
 }
---
+++ learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/node/Node.java
@@ -0,0 +1,16 @@
+package com.tomgs.scheduler.quartz.customer.node;
+
+import lombok.Data;
+
+/**
+ * @author tangzy
+ * @since 1.0
+ */
+@Data
+public class Node {
+
+  private String name;
+
+  private String ip;
+
+}
