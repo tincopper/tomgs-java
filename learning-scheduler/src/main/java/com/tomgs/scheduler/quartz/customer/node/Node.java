e87258b99d3426c724de00d6f9069d615dda0665
zy_tang <zy_tang@kingdee.com>
2020-05-21 22:09:07 +0800

    scheduelr 任务分配

--- learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/node/Node.java
+++ learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/node/Node.java
@@ -1,12 +1,16 @@
 package com.tomgs.scheduler.quartz.customer.node;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author tangzy
  * @since 1.0
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Node {
 
   private String name;
