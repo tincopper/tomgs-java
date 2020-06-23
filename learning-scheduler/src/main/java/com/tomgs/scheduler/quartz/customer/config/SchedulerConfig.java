aa5cee99de62df5a973f5c2dfdb6e75eb79bb06a
tomgs <tincopper@foxmail.com>
2020-06-24 00:12:18 +0800

    测试暂停恢复

--- learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/config/SchedulerConfig.java
+++ learning-scheduler/src/main/java/com/tomgs/scheduler/quartz/customer/config/SchedulerConfig.java
@@ -1,40 +1,18 @@
 package com.tomgs.scheduler.quartz.customer.config;
 
 import lombok.Builder;
+import lombok.Getter;
 
 /**
  * scheduler config
  * @author tangzy
  */
 @Builder
+@Getter
 public class SchedulerConfig {
 
   private String schedulerName;
   private int threadCount;
   private int misfireThreshold;
-
-  public int getThreadCount() {
-    return threadCount;
-  }
-
-  public void setThreadCount(int threadCount) {
-    this.threadCount = threadCount;
-  }
-
-  public String getSchedulerName() {
-    return schedulerName;
-  }
-
-  public void setSchedulerName(String schedulerName) {
-    this.schedulerName = schedulerName;
-  }
-
-  public int getMisfireThreshold() {
-    return misfireThreshold;
-  }
-
-  public void setMisfireThreshold(int misfireThreshold) {
-    this.misfireThreshold = misfireThreshold;
-  }
 
 }
