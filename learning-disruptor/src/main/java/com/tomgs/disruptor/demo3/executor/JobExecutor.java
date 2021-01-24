adc8899cca2a4f16897c0397c8027b9927e1adc2
tincopper <136082619@qq.com>
2021-01-24 21:09:38 +0800

    update job executor

--- learning-disruptor/src/main/java/com/tomgs/disruptor/demo3/executor/JobExecutor.java
+++ learning-disruptor/src/main/java/com/tomgs/disruptor/demo3/executor/JobExecutor.java
@@ -1,6 +1,7 @@
 package com.tomgs.disruptor.demo3.executor;
 
 import com.tomgs.disruptor.demo3.CircleQueue;
+
 import java.util.Objects;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -15,83 +16,86 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class JobExecutor {
 
-  private final ReentrantLock lock = new ReentrantLock();
+    private final ReentrantLock lock = new ReentrantLock();
 
-  private AtomicBoolean isStarted = new AtomicBoolean(false);
+    private AtomicBoolean executable = new AtomicBoolean(false);
 
-  private CircleQueue<JobRunnable> circleQueue;
+    private CircleQueue<JobRunnable> circleQueue;
 
-  private Executor executor;
+    private Executor executor;
 
-  public JobExecutor() {
+    public JobExecutor() {
 
-  }
-
-  public JobExecutor(final CircleQueue<JobRunnable> circleQueue, final Executor executor) {
-    this.circleQueue = circleQueue;
-    this.executor = executor;
-  }
-
-  public CircleQueue<JobRunnable> getCircleQueue() {
-    return circleQueue;
-  }
-
-  public void setCircleQueue(
-      CircleQueue<JobRunnable> circleQueue) {
-    this.circleQueue = circleQueue;
-  }
-
-  public Executor getExecutor() {
-    return executor;
-  }
-
-  public void setExecutor(Executor executor) {
-    this.executor = executor;
-  }
-
-  public void submit(JobRunnable runnable) {
-    lock.lock();
-    try {
-      boolean offered = circleQueue.offer(runnable);
-      if (!offered) {
-        System.out.println("queue offer false. 请检查任务入队策略...");
-        return;
-      }
-    } finally {
-      lock.unlock();
     }
 
-    // 从第一个任务触发，后续自动执行
-    if (isStarted.compareAndSet(false, true) && circleQueue.size() >= 1) {
-      notifyNextJobExecute();
+    public JobExecutor(final CircleQueue<JobRunnable> circleQueue, final Executor executor) {
+        this.circleQueue = circleQueue;
+        this.executor = executor;
     }
 
-  }
-
-  private void restart() {
-    isStarted.compareAndSet(true, false);
-  }
-
-  /**
-   * 通知下一个任务开始执行
-   */
-  private void notifyNextJobExecute() {
-    if (circleQueue.isEmpty()) {
-      // 对于空队列再添加元素时需要重新开始消费，否则将会导致队列任务不消费
-      restart();
-      return;
+    public CircleQueue<JobRunnable> getCircleQueue() {
+        return circleQueue;
     }
 
-    JobRunnable runnable = Objects.requireNonNull(circleQueue.poll());
-    executor.execute(() -> {
-      try {
-        runnable.run();
-      } catch (Exception e) {
-        runnable.exceptionCaught(e);
-      } finally {
-        notifyNextJobExecute();
-      }
-    });
-  }
+    public void setCircleQueue(CircleQueue<JobRunnable> circleQueue) {
+        this.circleQueue = circleQueue;
+    }
+
+    public Executor getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(Executor executor) {
+        this.executor = executor;
+    }
+
+    public void submit(JobRunnable runnable) {
+        lock.lock();
+        try {
+            boolean offered = circleQueue.offer(runnable);
+            if (!offered) {
+                System.out.println("queue offer false. 请检查任务入队策略...");
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        // 从第一个任务触发，后续自动执行
+        if (executable.compareAndSet(false, true) && circleQueue.size() >= 1) {
+            notifyNextJobExecute();
+        }
+
+    }
+
+    /**
+     * 重置可执行标识位
+     */
+    private void reset() {
+        executable.compareAndSet(true, false);
+    }
+
+    /**
+     * 通知下一个任务开始执行
+     */
+    private void notifyNextJobExecute() {
+        if (circleQueue.isEmpty()) {
+            // 对于空队列再添加元素时需要重新开始消费，否则将会导致队列任务不消费
+            reset();
+            return;
+        }
+
+        JobRunnable runnable = Objects.requireNonNull(circleQueue.poll());
+        executor.execute(() -> {
+            try {
+                runnable.run();
+            } catch (Exception e) {
+                runnable.exceptionCaught(e);
+            } catch (Throwable unexpected) {
+                System.out.println("意想不到的错误:" + unexpected);
+            } finally {
+                notifyNextJobExecute();
+            }
+        });
+    }
 
 }
--- learning-disruptor/src/main/java/com/tomgs/disruptor/demo3/executor/JobExecutorTest.java
+++ learning-disruptor/src/main/java/com/tomgs/disruptor/demo3/executor/JobExecutorTest.java
@@ -2,11 +2,11 @@ package com.tomgs.disruptor.demo3.executor;
 
 import com.tomgs.disruptor.demo3.CircleQueue;
 import com.tomgs.disruptor.demo3.ResizeQueuePolicy;
+import org.junit.Test;
+
 import java.io.IOException;
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import org.junit.Test;
 
 /**
  * @author tomgs
@@ -14,30 +14,30 @@ import org.junit.Test;
  */
 public class JobExecutorTest {
 
-  @Test
-  public void testJobExecutor() throws IOException {
+    @Test
+    public void testJobExecutor() throws IOException {
 
-    ExecutorService demoExecutor = Executors.newFixedThreadPool(10);
+        ExecutorService demoExecutor = Executors.newFixedThreadPool(10);
 
-    CircleQueue<JobRunnable> queue = new CircleQueue<>(16, new ResizeQueuePolicy<>());
-    ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(10);
-    JobExecutor executor = new JobExecutor();
-    executor.setCircleQueue(queue);
-    executor.setExecutor(threadPoolExecutor);
+        CircleQueue<JobRunnable> queue = new CircleQueue<>(16, new ResizeQueuePolicy<>());
+        ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(10);
+        JobExecutor executor = new JobExecutor();
+        executor.setCircleQueue(queue);
+        executor.setExecutor(threadPoolExecutor);
 
-    for (int i = 0; i < 100; i++) {
-      int finalI = i;
-      executor.submit(new JobRunnable() {
-        @Override
-        public void execute() throws Exception {
-          System.out.println("" + finalI);
-          //Thread.sleep(new Random().nextInt(2000));
-          //System.out.println("---------");
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            executor.submit(new JobRunnable() {
+                @Override
+                public void execute() throws Exception {
+                    System.out.println("" + finalI);
+                    //Thread.sleep(new Random().nextInt(2000));
+                    //System.out.println("---------");
+                }
+            });
         }
-      });
-    }
 
-    System.in.read();
-  }
+        System.in.read();
+    }
 
 }
