48383a65abbdd6424028cbea90ecd84003be05cc
tomgs <136082619@qq.com>
2021-05-31 11:04:36 +0800

    add kafka demo

---
+++ learning-kafka/src/main/java/com/tomgs/kafka/consumer/ConsumerTest.java
@@ -0,0 +1,18 @@
+package com.tomgs.kafka.consumer;
+
+import org.junit.Test;
+
+/**
+ * 消费者测试
+ *
+ * @author tomgs
+ * @since 2021/5/31
+ */
+public class ConsumerTest {
+
+  @Test
+  public void testConsume1() {
+
+  }
+
+}
---
+++ learning-kafka/src/main/java/com/tomgs/kafka/producer/ProducerTest.java
@@ -0,0 +1,18 @@
+package com.tomgs.kafka.producer;
+
+import org.junit.Test;
+
+/**
+ * 生产者测试
+ *
+ * @author tomgs
+ * @since 2021/5/31
+ */
+public class ProducerTest {
+
+  @Test
+  public void testProducer1() {
+
+  }
+
+}
