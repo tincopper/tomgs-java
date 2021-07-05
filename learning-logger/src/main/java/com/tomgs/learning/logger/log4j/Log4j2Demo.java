3074c6d8bc501f9cf73d49a76e77a533b2c6c977
tincopper <136082619@qq.com>
2021-07-06 00:06:04 +0800

    add note

--- learning-logger/src/main/java/com/tomgs/learning/logger/log4j/Log4j2Demo.java
+++ learning-logger/src/main/java/com/tomgs/learning/logger/log4j/Log4j2Demo.java
@@ -15,7 +15,6 @@ import org.apache.logging.log4j.core.config.AppenderRef;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.filter.LevelRangeFilter;
-import org.apache.logging.log4j.core.filter.ThresholdFilter;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 
 import java.nio.charset.Charset;
