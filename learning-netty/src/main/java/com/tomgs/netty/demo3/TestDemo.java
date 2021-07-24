d4be59c11d186e3329d7957bca7962a8005ef6e8
tincopper <136082619@qq.com>
2021-07-24 22:35:39 +0800

    add test

---
+++ learning-netty/src/main/java/com/tomgs/netty/demo3/TestDemo.java
@@ -0,0 +1,15 @@
+package com.tomgs.netty.demo3;
+
+/**
+ * test demo
+ * 
+ * @author tomgs
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("321");
+    }
+
+}
