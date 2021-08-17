e40ac164af89d1eafa551cb11f9c9cba584a970d
tincopper <136082619@qq.com>
2021-08-17 23:49:14 +0800

    add test

--- learning-netty/src/main/java/com/tomgs/netty/demo3/TestDemo.java
+++ learning-netty/src/main/java/com/tomgs/netty/demo3/TestDemo.java
@@ -7,9 +7,16 @@ package com.tomgs.netty.demo3;
  */
 public class TestDemo {
 
+    private static final String CONSTANT_NAME = "123";
+
     public static void main(String[] args) {
+        int a = 123;
+        String str = "abc";
+        
+        System.out.println(a);
         System.out.println("123");
         System.out.println("321");
+        System.out.println(CONSTANT_NAME);
     }
 
 }
