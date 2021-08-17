a6791ee12e2e6ee39867edd248ef1a2c764a076f
zy_tang <zy_tang@kingdee.com>
2021-08-17 21:00:58 +0800

    add zk demo

---
+++ learning-zk/src/main/java/com/tomgs/zk/demo/ZkClientDemo.java
@@ -0,0 +1,50 @@
+package com.tomgs.zk.demo;
+
+import com.google.common.base.Charsets;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author tomgs
+ * @since 2021/4/19
+ */
+public class ZkClientDemo {
+
+  private CuratorFramework zkClient;
+
+  @Before
+  public void before() {
+    String zkServerAddress = "127.0.0.1:2181";
+    ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3, 5000);
+    zkClient = CuratorFrameworkFactory.builder()
+        .namespace("elog")
+        .connectString(zkServerAddress)
+        .sessionTimeoutMs(5000)
+        .connectionTimeoutMs(5000)
+        .retryPolicy(retryPolicy)
+        .build();
+    zkClient.start();
+  }
+
+  @Test
+  public void createPath() throws Exception {
+    String parentDir = "/erms/datasource";
+    if (zkClient.checkExists().forPath(parentDir) == null) {
+      zkClient.create().creatingParentsIfNeeded().forPath(parentDir);
+//      zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(parentDir);
+    }
+  }
+
+  @Test
+  public void testCreatePersistentSequential() throws Exception {
+    String key = "/test/event";
+    String value = "123";
+    zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT_SEQUENTIAL).forPath(key, value.getBytes(Charsets.UTF_8));
+    zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT_SEQUENTIAL).forPath(key, value.getBytes(Charsets.UTF_8));
+  }
+
+}
