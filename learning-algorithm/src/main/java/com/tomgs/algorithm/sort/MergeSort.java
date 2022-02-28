0c56753fcec785c0566e22d5d37c7ce2e8cd22b6
tincopper <136082619@qq.com>
2022-02-28 22:59:26 +0800

    add 2 lc demo

---
+++ learning-algorithm/src/main/java/com/tomgs/algorithm/sort/MergeSort.java
@@ -0,0 +1,30 @@
+package com.tomgs.algorithm.sort;
+
+import org.junit.Test;
+
+/**
+ * 如何在 O(n) 的时间复杂度内查找一个无序数组中的第 K 大元素？
+ *
+ * 如果要排序一个数组，我们先把数组从中间分成前后两部分，然后对前后两部分分别排序，
+ * 再将排好序的两部分合并在一起，这样整个数组就都有序了。
+ *
+ * https://static001.geekbang.org/resource/image/db/2b/db7f892d3355ef74da9cd64aa926dc2b.jpg
+ *
+ * 归并排序使用的就是分治思想。分治，顾名思义，就是分而治之，将一个大问题分解成小的子问题来解决。
+ * 小的子问题解决了，大问题也就解决了。
+ *
+ * @author tomgs
+ * @version 2022/2/28 1.0
+ */
+public class MergeSort {
+
+    public void mergeSort() {
+
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+}
--- learning-algorithm/src/main/java/com/tomgs/algorithm/sort/QuickSort.java
+++ learning-algorithm/src/main/java/com/tomgs/algorithm/sort/QuickSort.java
@@ -11,5 +11,26 @@ public class QuickSort {
     public static void main(String[] args) {
 
     }
-    
+
+    // 王道考研书上看到的快排算法，利用哨兵减少了交换两个元素的复杂步骤，效果更好一些
+    private static void quickSort(int[] a, int head, int tail) {
+
+        int low = head;
+        int high = tail;
+        int pivot = a[low];
+        if (low < high) {
+
+            while (low<high) {
+                while (low < high && pivot <= a[high]) high--;
+                a[low] = a[high];
+                while (low < high && pivot >= a[low]) low++;
+                a[high]=a[low];
+            }
+            a[low] = pivot;
+
+            if(low>head+1) quickSort(a,head,low-1);
+            if(high<tail-1) quickSort(a,high+1,tail);
+        }
+
+    }
 }
