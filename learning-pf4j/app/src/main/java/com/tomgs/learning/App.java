253f17cc85765d675c27963981aa6e94a6185d0f
tincopper <136082619@qq.com>
2022-11-25 11:16:12 +0800

    添加插件

--- learning-pf4j/app/src/main/java/com/tomgs/learning/App.java
+++ learning-pf4j/app/src/main/java/com/tomgs/learning/App.java
@@ -1,11 +1,11 @@
 package com.tomgs.learning;
 
+import cn.hutool.core.util.ReflectUtil;
 import com.tomgs.learning.api.Greeting;
-import org.pf4j.DefaultPluginManager;
 import org.pf4j.JarPluginManager;
-import org.pf4j.PluginManager;
 
-import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         //使用默认的插件管理器
         //PluginManager pluginManager = new DefaultPluginManager(new File("learning-pf4j/plugins/greeting-plugin/target/").toPath());
         //加载插件
@@ -26,15 +26,29 @@ public class App {
 
         //启动插件
         pluginManager.startPlugin("greeting-plugin");
+        //final ClassLoader pluginClassLoader = pluginManager.getPluginClassLoader("greeting-plugin");
+        //pluginClassLoader.loadClass("com.tomgs.learning.api.Greeting");
 
         //获取所有的Greeting的扩展实现
-        List<Greeting> greetings = pluginManager.getExtensions(Greeting.class, "greeting-plugin");
+        final List<?> greetings = pluginManager.getExtensions("greeting-plugin");
         if (greetings.size() == 0) {
             System.err.println("未找到插件");
         }
-        for (Greeting greeting : greetings) {
-            System.out.println( greeting.message("张三"));
-        }
+        //for (Greeting greeting : greetings) {
+        //    System.out.println( greeting.message("张三"));
+        //}
+        greetings.forEach(e -> {
+            final Object result = ReflectUtil.invoke(e, "message", "tomgs");
+            System.out.println(result);
+            // or
+            /*try {
+                final Method message = e.getClass().getMethod("message", String.class);
+                final Object result = message.invoke(e, "tomgs");
+                System.out.println(result);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex) {
+                throw new RuntimeException(ex);
+            }*/
+        });
     }
 
 }
--- learning-pf4j/app/src/test/java/com/tomgs/learning/AppTest.java
+++ learning-pf4j/app/src/test/java/com/tomgs/learning/AppTest.java
@@ -7,32 +7,30 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws ClassNotFoundException {
+        final Class<?> superClass = Class.forName("com.tomgs.learning.api.Greeting");
+        final Class<?> subClass = Class.forName("com.tomgs.learning.plugin.ChineseGreeting");
+        System.out.println(superClass.isAssignableFrom(subClass));
     }
 }
