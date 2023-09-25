1668dbd9e248b5f63e7ee1190fd8bb25d08ee311
tincopper <136082619@qq.com>
2023-09-25 10:29:31 +0800

    SSE demo测试

--- learning-springboot/src/main/java/com/tomgs/springboot/controller/UserController.java
+++ learning-springboot/src/main/java/com/tomgs/springboot/controller/UserController.java
@@ -7,10 +7,16 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author tangzhongyuan
@@ -34,6 +40,31 @@ public class UserController {
     @GetMapping("user/{id}")
     public User getUserById(@PathVariable(value = "id") Integer id) {
         return userMapper.selectById(id);
+    }
+
+    @GetMapping(value = "/sse")
+    @ResponseBody
+    public String getMessage(HttpServletResponse response) {
+        // System.out.println("请求进入了");
+        response.setContentType("text/event-stream");   // 指定ContentType，不可变
+        response.setCharacterEncoding("utf-8");         // 指定响应字符集，是否可变，没测试，但建议指定utf-8
+        while (true) {
+            String s = "";
+            s += "retry: 5000\n";                   // 客户端没有获取到数据，就不断发送新的请求（间隔5秒），直到有数据。
+            s += "id: " + UUID.randomUUID() + "\n"; // 这里指定消息ID
+            s += "event: eventType\n";              // 这里定义事件类型，自定义！
+            s += "data: " + new Date() + "\n\n";    // 这里设置返回的数据
+            try {
+                PrintWriter pw = response.getWriter();
+                Thread.sleep(1000L);
+                pw.write(s);
+                pw.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
--- learning-springwebflux/src/main/java/com/tomgs/springboot/webflux/controller/TestController.java
+++ learning-springwebflux/src/main/java/com/tomgs/springboot/webflux/controller/TestController.java
@@ -1,8 +1,15 @@
 package com.tomgs.springboot.webflux.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.LocalTime;
 
 /**
  * @author tomgs
@@ -19,6 +26,35 @@ public class TestController {
   @GetMapping("/hello1")
   public String hello1() {
     return "Hello world1";
+  }
+
+  // https://www.baeldung.com/spring-server-sent-events
+  @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<String> streamFlux() {
+    return Flux.interval(Duration.ofSeconds(1))
+            .map(sequence -> "Flux - " + LocalTime.now().toString());
+  }
+
+  @GetMapping(path = "/stream-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<ServerSentEvent<String>> streamEvents() {
+    return Flux.interval(Duration.ofSeconds(1))
+            .map(sequence -> {
+              if (sequence % 2 == 0) {
+                return ServerSentEvent.<String> builder()
+                        .id(String.valueOf(sequence))
+                        .retry(Duration.ofSeconds(3))
+                        .event("diyEventType")
+                        .data("SSE - " + LocalTime.now().toString())
+                        .build();
+              }
+              // default event
+              return ServerSentEvent.<String> builder()
+                      .id(String.valueOf(sequence))
+                      .retry(Duration.ofSeconds(3))
+                      .event("message")
+                      .data("SSE - " + LocalTime.now().toString())
+                      .build();
+            });
   }
 
 }
